a26f9c07870b80bdfefd55236de501a5a6b6b733
Rafael Campos Nunes <rafaelnunes@engineer.com>
2023-04-12 23:53:50 -0300

    Add other artifacts to the game (BGM and images)
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- tux/src/main.cpp
+++ tux/src/main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 
 #include "core/game.hpp"
+#include "core/sound/track.hpp"
+#include "graphics/sdl/sprite.hpp"
 
 int main() {
-    Kokiri::Core::Game game("Rafael Campos Nunes - 19/0098295", 800, 600);
+    using namespace Kokiri::Core;
+    using namespace Kokiri::Graphics::SDL;
+
+    Game game("Rafael Campos Nunes - 19/0098295", 1024, 600);
+
+    Track bgm("assets/audio/stateStage.ogg");
+    Track effect("assets/audio/boom.wav");
+
+    Sprite sprite_tux(game.get_window(), "assets/img/ocean.jpg");
+
+    sprite_tux.render(0, 0);
 
     game.loop();
+
+    effect.play(1);
+    bgm.play(1); // not working over here (linux)
 
     return 0;
 }
