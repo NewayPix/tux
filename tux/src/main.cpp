64990be2983c9e624cd33fa93cf98e33935b8911
Rafael Campos Nunes <rafaelnunes@engineer.com>
2023-05-15 19:04:37 -0300

    Add conditional sprite creation
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- tux/src/main.cpp
+++ tux/src/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <utility>
+#include <vector>
 
 #include "core/game.hpp"
+#include "core/event.hpp"
+#include "core/types.hpp"
+#include "core/functions.hpp"
 #include "core/sound/track.hpp"
+#include "core/debug/log.hpp"
+
 #include "graphics/sdl/sprite.hpp"
 
 int main() {
-    using namespace Kokiri::Core;
+    using namespace Kokiri;
     using namespace Kokiri::Graphics::SDL;
 
     Game game("Rafael Campos Nunes - 19/0098295", 1024, 600);
@@ -13,17 +20,48 @@ int main() {
     Track bgm("assets/audio/stageState.ogg");
     Track effect("assets/audio/boom.wav");
 
-    auto render = [&game](){
-        Sprite sprite_tux(game.get_window(), "assets/img/ocean.jpg");
-        sprite_tux.render(0,0);
+    std::vector<std::pair<v2<int>, Sprite*>> assets;
+
+    Sprite background(game.get_window(), "assets/img/ocean.jpg");
+
+    // user render function
+    auto render = [&game, &background, &assets](){
+        background.render(0, 0);
+
+        for (const auto& asset : assets) {
+            auto p = asset.first;
+            auto s = asset.second;
+
+            s->render(p.x, p.y);
+        }
     };
 
+    // user event function
+    auto event = [&game, &assets]() {
+        auto e = game.get_event();
+
+        if (e.get()->is_mouse_click(Event::Mouse::LeftButton)) {
+            auto p = e.get()->get_mouse_position();
+            auto s = new Sprite(game.get_window(), "assets/img/penguin.png");
+
+            Log::info("click: ", p.x, p.y);
+
+            assets.push_back(std::make_pair(p, s));
+        }
+    };
+
+    game.bind(FunctionType::Event, event);
     game.bind(FunctionType::Render, render);
 
     effect.play(-1);
-    bgm.play(1); // not working over here (linux)
+    bgm.play(1);
 
     game.loop();
+
+    // clean everything
+    for (const auto& asset : assets) {
+        delete asset.second;
+    }
 
     return 0;
 }
