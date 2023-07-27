dd5662662d5310bbe65659c409906ed21f37f67d
Rafael Campos Nunes <rafaelnunes@engineer.com>
2023-07-27 12:02:23 -0300

    Improve build script
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- compile.sh
+++ compile.sh
@@ -5,23 +5,42 @@
 #
 
 BASE=$(pwd)
+BUILD="${BASE}/build"
+
+KOKIRI_LOGS="${BUILD}/kokiri.log"
+TUX_LOGS="${BUILD}/tux.log"
+
+prepare() {
+    mkdir -p "${BUILD}"
+    cp -r ./tux/assets "${BUILD}"
+}
 
 compile_engine() {
-    cd vendor/kokiri || return 1
+    echo "COMPILING KOKIRI..."
 
-    premake gmake
-    make
+    cd ./vendor/kokiri || return 1
+
+    premake gmake > /dev/null
+    make > "${KOKIRI_LOGS}" 2>&1 || return 1
 
     cd "${BASE}" || return 1
 }
 
 compile_game() {
-    premake gmake
+    echo "COMPILING TUX..."
+
+    premake gmake > /dev/null
 
     cd tux || return 1
 
-    make
+    make > "${TUX_LOGS}" 2>&1 || return 1
+
+    cp ./build/tux/tux "${BASE}/build/"
+    cd "${BASE}" || return 1
 }
+
+
+prepare
 
 compile_engine
 compile_game
